1f1152009fc9774661df6fdeb06d9de174de3b8e
luluaw4 <luluaprilia1604@gmail.com>
2023-07-10 20:13:15 +0700

    MENAMBAHKAN FOLDER PRAKTIKUM 9-14

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,33 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/after_registrasi', function () {
+    return view('after_registrasi');
+});
 
 Route::get('/profile', function () {
     return view('profile');
 });
 
+Route::get('/output', function () {
+    return view('output');
+});
+
+Route::get('/input', function () {
+    return view('input');
+});
+
+Route::get('/form', function () {
+    return view('form');
+});
+
+use App\Http\Controllers\InputController;
+
+    Route::get('/input',
+    [InputController::class, 'index']);
+
+    Route::post('/output',
+    [InputController::class, 'output']); 
 
 use App\Http\Controllers\TokoController;
 
@@ -35,8 +57,61 @@ Route::prefix('toko')->group(function(){
         [TokoController::class, 'detail']);
 
     Route::get('/about',
-        [TokoController::class, 'about']);  
+        [TokoController::class, 'about']); 
+
+    Route::group(['middleware' => ['auth']], function(){
+        
+        Route::get('/admin',
+        [TokoController::class, 'admin'])->name('produk.admin');
+
+        Route::get('/create',
+        [TokoController::class, 'create'])->name('produk.create');  
+    
+        Route::post('/store',
+        [TokoController::class, 'store'])->name('produk.store');    
+
+
+        Route::get('/customers',
+        [TokoController::class, 'customers'])->name('produk.customers');
+
+        Route::get('/AddCustomer',
+        [TokoController::class, 'AddCustomer'])->name('produk.AddCustomer');  
+    
+        Route::post('/',
+        [TokoController::class, 'NewCustomer'])->name('produk.NewCustomer');  
+
+
+        Route::get('/{product}/edit',
+        [TokoController::class, 'edit'])->name('produk.edit');    
+
+        Route::delete('/{product}',
+        [TokoController::class, 'destroy'])->name('produk.destroy'); 
+
+        Route::put('/{product}',
+        [TokoController::class, 'update'])->name('produk.update'); 
+
+
+
+        Route::get('/{customer}/editcustomer',
+        [TokoController::class, 'editcustomer'])->name('pelanggan.editcustomer');    
+
+        Route::delete('/{customer}/destroycustomer',
+        [TokoController::class, 'destroycustomer'])->name('pelanggan.destroycustomer'); 
+
+        Route::put('/{customer}/updatecustomer',
+        [TokoController::class, 'updatecustomer'])->name('pelanggan.updatecustomer'); 
+
+        Route::group(['middleware' => ['auth', 'peran:admin-manager']], function(){
+        });
+        
+
+
+    });    
+    
+  
 
 });
 
+Auth::routes();
 
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
